a757858d3ed9b90055cffe2acc8e1e7ca6a0e24d
Dima2323-tech <123169116+PavlovWorkerAcc@users.noreply.github.com>
2023-08-10 18:19:03 +0300

    Update Me.tsx

---
+++ src/Components/Me/Me.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Photo from "../../img/Me.png"
+
+export default function Me() {
+
+    return (
+
+        <div className="Me_Container text-white w-11/12 mt-40 ml-auto mr-auto flex flex-col">
+
+            <div className="Me_Banner flex justify-between items-center">
+
+                <div className="Me_Text_Container">
+
+                    <h1>Hey Ther</h1>
+
+                </div>
+                
+                <div className="Me_Photo_Container w-2/5">
+
+                    <img className="" src={Photo}/>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
